3a3b01ef6dd1a09e5db1c91aaa90ea63ea06c561
Fran Castillo <castillofranciscodaniel@gmail.com>
2022-01-09 04:52:01 -0300

    Test_Post_should_return_errors_if_the_id_exist add

--- domain/beerService_test.go
+++ domain/beerService_test.go
@@ -66,19 +66,21 @@ func makeCurrencies() provider.Currencies {
 
 }
 
-//func Test_Post_should_return_errors_if_the_new_beer_can_not_be_created(t *testing.T) {
-//	tearDown := setUp(t)
-//	defer tearDown()
-//
-//	beer, _ := NewBeer(2, "Patagonia", "Norte", "Chile", 740, "CL")
-//
-//	mockBeerRepository.EXPECT().Post(beer).Return(nil, err.NotFoundError)
-//
-//	beerService.Post(beer)
-//
-//	t.Error("Failed while testing the status code")
-//
-//}
+func Test_Post_should_return_errors_if_the_id_exist(t *testing.T) {
+	tearDown := setUp(t)
+	defer tearDown()
+
+	beer, _ := makeBeerUsd()
+
+	mockBeerRepository.EXPECT().Post(beer).Return(err2.DuplicatedIdError)
+
+	err := beerService.Post(beer)
+
+	if err != nil && err != err2.DuplicatedIdError {
+		t.Error("the error should be: ", err.Error())
+	}
+
+}
 
 func Test_BoxPrice_same_visa(t *testing.T) {
 	tearDown := setUp(t)
